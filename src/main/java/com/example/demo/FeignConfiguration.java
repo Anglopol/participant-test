6245002782db4b0abe66a0b1eb4ed381565faaec
Ivan Zelenkov <ivan_zelenkov@dell.com>
2020-08-19 22:04:19 +0300

    test webhook

--- src/main/java/com/example/demo/FeignConfiguration.java
+++ src/main/java/com/example/demo/FeignConfiguration.java
@@ -14,6 +14,7 @@ public class FeignConfiguration {
     @Value("${service.app.host}")
     private String serviceAppHost;
 
+
     @Bean
     public KafkaServiceClient getKafkaServiceClient() {
         return Feign.builder()
